d02c248208f8200d84900606b9ae7f8df133d76b
tmwsiy <tmwsiy@152.20.234.225>
2015-04-16 08:37:29 -0400

    asd

---
+++ greektext/src/com/eddiedunn/greek/test/TestDataReader.java
@@ -0,0 +1,16 @@
+package com.eddiedunn.greek.test;
+
+import com.eddiedunn.greek.data.WordDataReader;
+
+public class TestDataReader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		WordDataReader wdr = new WordDataReader();
+		wdr.readDataFiles();
+
+	}
+
+}
